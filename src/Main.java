28d8046372a3c13e59d80d7d4e7a757a039c186f
Mehmet <mehmetsonmez216@gmail.com>
2023-12-01 16:59:04 +0300

    Employee finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import com.workintech.company.Employee;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Employee> employeeList=new LinkedList<>();
+        employeeList.add(new Employee
+                (1,"Mehmet","Sonmez"));
+        employeeList.add(new Employee
+                (1,"ahmet","Sonmez"));
+        employeeList.add(new Employee
+                (4,"Ali","Veli"));
+        employeeList.add(new Employee
+                (4,"Yunus","Öz"));
+        employeeList.add(new Employee
+                (5,"Ugurcan","Yıldız"));
+        employeeList.add(new Employee
+                (6,"Umut","Çavuş"));
+
+
+        System.out.println(employeeList);
+
+
+
+        List<Employee> duplicateList=new LinkedList<>();
+        Map<Integer,Employee> uniqueMap=new HashMap<>();
+
+
+        for(Employee employee: employeeList){
+            if (uniqueMap.containsKey(employee.getId())){
+                duplicateList.add(employee);
+            }else {
+                uniqueMap.put(employee.getId(),employee);
+            }
+
+        }
+        employeeList.removeAll(duplicateList);
+
+        System.out.println("*****************");
+        System.out.println("duplicate"+duplicateList);
+
     }
 }
